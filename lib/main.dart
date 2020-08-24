165ee1df59a5bbb7918a6661bd152f881d02cd2c
Mike <mferrari@swiftcom.com>
2020-08-24 07:34:19 -0700

    added SafeArea, removed AppBar, updated button styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -52,19 +51,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -73,49 +59,138 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    final TextStyle display1 = Theme.of(context).textTheme.headline4;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: SafeArea(
+        // left: false,
+        // top: false,
+        // right: false,
+        // bottom: false,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            topbar,
+            languagechooser,
+            logo,
+            signinform,
+            forgotpassword,
+            signuplink,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget topbar = Container(
+  decoration: BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.bottomRight,
+      end: Alignment.bottomLeft,
+      colors: [Colors.red, Colors.pink],
+    ),
+  ),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              'You have pushed the button this many times:',
-              style: GoogleFonts.oswald(textStyle: display1,),
+              'IG Clone',
+              style: GoogleFonts.oswald(
+                color: Colors.white,
+              ),
+              softWrap: true,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Find it for free on Google Play.',
+              style: GoogleFonts.oswald(
+                color: Colors.white,
+              ),
+              softWrap: true,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+      Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 10.0, 10.0),
+            child: OutlineButton(
+              onPressed: () {},
+              child: Text(
+                'GET',
+                style: GoogleFonts.oswald(
+                  color: Colors.white,
+                ),
+                softWrap: true,
+              ),
+              borderSide: BorderSide(color: Colors.white),
+
+              // shape: StadiumBorder(),
+            ),
+            // RaisedButton(
+            //   color: Colors.red,
+            //   onPressed: () {},
+            //   child: Text(
+            //     'GET',
+            //     style: GoogleFonts.oswald(color: Colors.white,),
+            //     softWrap: true,
+            //   ),
+            // ),
+          ),
+        ],
+      ),
+    ],
+  ),
+);
+
+Widget languagechooser = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'English',
+    style: GoogleFonts.oswald(),
+    softWrap: true,
+  ),
+);
+
+Widget logo = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Logo',
+    style: GoogleFonts.oswald(),
+    softWrap: true,
+  ),
+);
+
+Widget signinform = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Registration Form',
+    style: GoogleFonts.oswald(),
+    softWrap: true,
+  ),
+);
+
+Widget forgotpassword = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Forgot password',
+    style: GoogleFonts.oswald(),
+    softWrap: true,
+  ),
+);
+
+Widget signuplink = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Logo',
+    style: GoogleFonts.oswald(),
+    softWrap: true,
+  ),
+);
